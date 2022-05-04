1231199dc2de09dacd64600503f204d217dca0bd
manishmanchegowda <manish2@pdx.edu>
2022-05-04 12:48:13 -0700

    added code for array.js

--- hw4/02-arrays.js
+++ hw4/02-arrays.js
@@ -2,24 +2,38 @@ let result = [];
 
 const insertItem = (item) => {
   // insert item into result
-
+  result.push(item);
   return item;
 };
 
 const deleteItem = (item) => {
   // remove the first occurrence of item in result
 
+  //find the index of an item
+  let pos = result.indexOf(item);
+
+  //Remove an item by index position
+  result.splice(pos, 1);
   return item;
 };
 
 const lookupItem = (index) => {
   // return the item from result at index, deleting the obtained item
+  let indexValue = result[index];
 
+  //Remove an item by index position
+  let pos1 = result.indexOf(indexValue);
+  if (index >= 0) {
+    result.splice(pos1, 1);
+    return indexValue;
+  } 
+  else return "null";
   return index;
 };
 
 const printItems = () => {
   // return a string of the concatenated item in result, separated by commas
+  return result.join(", ");
 };
 
 insertItem(1);
