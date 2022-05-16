3ed96c2f6aafdc1ad9da0d8bbbf47f7e15bc626c
manishmanchegowda <manish2@pdx.edu>
2022-05-15 19:17:38 -0700

    added code for 03- bevarages

--- hw5/03-beverages.js
+++ hw5/03-beverages.js
@@ -3,4 +3,77 @@ $(document).ready(function () {
     'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
 
   // Add your code here
+  const addItemToDOM = (drink) => {
+    console.log(drink.strDrink);
+    console.log(drink.strDrinkThumb);
+
+    $("#results").css({
+      gridTemplateColumns: "auto auto auto",
+      display: "grid",
+      textAlign: "center",
+      padding: "0px 50px",
+      gridRowGap: "10px",
+      gap: "10px",
+    });
+
+    $("#results").append(
+      $("<div>")
+        .addClass("figure")
+        .css({
+          width: "100%",
+          height: "100%",
+        })
+        .append(
+          $("<img>")
+            .css({
+              width: "100%",
+              height: "70%",
+            })
+            .attr("src", drink.strDrinkThumb)
+            .attr("alt", `${drink.strDrink}-${drink.idDrink}`)
+        )
+        .append(
+          $("<label>")
+            .css({
+              marginTop: "10px",
+              marginBottom: "50px",
+              fontWeight: "bold",
+              width: "70%",
+              display: "inline-block",
+            })
+            .text(drink.strDrink)
+        )
+    );
+  };
+
+  const fetchData = (url) => {
+    
+    $.ajax({
+      type: "GET",
+      url: url,
+      success: (data) => {
+        console.log(data);
+        if (data == undefined || data == null || data == "") {
+          $("#results").append($("<div>").text("Data not available"));
+        } else {
+          data.drinks.forEach((drink) => {
+            addItemToDOM(drink);
+          });
+        }
+      },
+
+      error: (error) => {
+        console.log(error);
+        $("#results").append(
+          $("<div>").text("An error occured. Please try again.")
+        );
+      },
+
+      complete: () => {
+        $("#loading").remove();
+      },
+    });
+  };
+
+  fetchData(url);
 });
